74d9fed5c811857215c75a6c6289b02e70d6abec
Davy <machadodavi3778@gmail.com>
2025-01-16 16:22:10 +0000

    Add files via upload

---
+++ Exemplo12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+public class exemplo12
+{
+    public static void Main(string[] args)
+    {
+        //Esta variavel e um contador que vai ate 10
+        int contador = 0;
+
+        //Logica do exercicio 12
+        do
+        {
+            Console.WriteLine(contador);
+            //Isto aumenta o numero do contador em 1
+            contador++;
+        }
+        while (contador <= 10);
+
+
+        
+
+    }
+}
---
+++ Exercicio11.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+public class Exercicio11
+{
+    public static void Main(string[] args)
+    {
+       int soma = 1;
+        int cont = 1;
+
+        while (soma <= 100)
+        {
+          cont++;
+            soma += cont;
+            Console.WriteLine(cont+" = "+soma);
+        }
+        
+    }
+}
