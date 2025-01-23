1d4eb06192c5736c4dab6f083923e9d974ba3892
Davy <machadodavi3778@gmail.com>
2025-01-23 17:19:27 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+public class Exercicio_15 
+{
+    public static void Main(string[] args) 
+    {
+        bool resultado = VddFls(1);
+
+        if (resultado == true)
+        {
+            Console.WriteLine("O numnero e par");
+        }
+        else
+        {
+            Console.WriteLine("O numero e impar");
+        }
+    }
+    static bool VddFls(int a)   
+    {
+        return a % 2 == 0;
+    }
+}
