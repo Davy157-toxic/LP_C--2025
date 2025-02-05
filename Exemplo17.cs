f481e8199027ef8f2886c9213ebe19d7af2128ec
Davy <machadodavi3778@gmail.com>
2025-02-05 17:19:45 +0000

    Add files via upload

---
+++ Exemplo17.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Exemplo17 
+{
+    public static void Main(string[] args) 
+    {
+        int[] IdadeAlunos = { 10 ,15, 17, 19};
+        //usando for
+        Console.WriteLine("Idade percorrida com for:");
+        for (int contador = 0; contador < IdadeAlunos.Length; contador++) 
+        {
+         Console.WriteLine(IdadeAlunos[contador]);
+        }
+        //usando foreach
+        Console.WriteLine("Idade percorrida com foreach: ");
+        foreach (int idade in IdadeAlunos) 
+        {
+            Console.WriteLine(idade);
+        }
+    }
+}
