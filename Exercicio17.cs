14ae27104505039c8ada4aa37c2e69a940737121
Davy <machadodavi3778@gmail.com>
2025-02-12 10:45:14 +0000

    Add files via upload

---
+++ Exercicio17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+public class Exercicio17
+{
+    public static void Main(string[] args) 
+    {
+        // Iniciar os arrays
+        String[] filmes = { "Harry Potter","Zorro","Simpsons"};
+        int[] datafilme = { 1999, 2010, 2014 };
+
+        Console.WriteLine("Lista de filmes\n");
+
+        // Percorre o array datafilme
+        for (int contador = 0; contador < datafilme.Length;contador++)
+        {
+            String f = filmes[contador];
+               Console.WriteLine(f + "-" + datafilme[contador]);
+        }  
+    }
+}
---
+++ Exercicio18.cs
@@ -0,0 +1,20 @@
+﻿using System;
+public class Exercicio18 
+{
+    public static void Main(String[] args) 
+    {
+        //Abrindo arrays parea Cidades e Habitantes 
+        string[] Cidade = { "Lisboa", "Porto", "Coimbra", "Faro", "Braga" };
+        int[] Habitantes = { 545000, 237000, 143000, 118000, 193000 };
+
+        Console.WriteLine("Cidades e as suas populaçãoes: ");
+        //Correndo a array Habitantes 
+        for (int cont = 0; cont < Habitantes.Length; cont++)
+        {
+            //juntando a array Cidaddes ao codigo 
+            String c = Cidade[cont];
+            Console.WriteLine($"{Cidade[cont]}-{Habitantes[cont]}");
+
+        }
+    }
+}
